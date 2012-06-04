e2606f332788d1490130744173dac51fb9ffc20d
Sebastian Ludwig <sebastian@lurado.de>
2012-06-05 00:13:13 +0200

    merged da0a31e6b4f31e92eb0760a4773d0f7216b4bfc3 and 01ed11180985adf858df906f456c7a2aff746798 from https://github.com/leniency/UnitySteer/  (couldn't figure out how else to give you credit, leniency..)
    "Changed out how wander works. It follows the original red3d.com examples with a steering circle. This allows for more predictable and controllable wander behavior. It also works better with the reflection that happens when hitting a tether boundary. Also fixed a redundant variable name in Tether."

--- Behaviors/SteerForWander.cs
+++ Behaviors/SteerForWander.cs
@@ -1,6 +1,6 @@
+#define ANNOTATE_WANDER
 using UnityEngine;
 using UnitySteer;
-using UnitySteer.Helpers;
 
 /// <summary>
 /// Steers a vehicle to wander around
@@ -8,56 +8,95 @@ using UnitySteer.Helpers;
 [AddComponentMenu("UnitySteer/Steer/... for Wander")]
 public class SteerForWander : Steering
 {
-	#region Private fields
-	float _wanderSide;
-	float _wanderUp;
-	
-	[SerializeField]
-	float _maxLatitudeSide = 2;
-	[SerializeField]
-	float _maxLatitudeUp = 2;
-	#endregion
-	
-	
-	#region Public properties
-	/// <summary>
-	/// Maximum latitude to use for the random scalar walk on the side
-	/// </summary>
-	public float MaxLatitudeSide {
-		get {
-			return this._maxLatitudeSide;
-		}
-		set {
-			_maxLatitudeSide = value;
-		}
-	}
+    Vector3 _jitter;            // Last jitter angle vector
+    float _angle;               // Current jitter angle off the velocity in degrees
 
-	/// <summary>
-	/// Maximum latitude to use for the random scalar walk on the up vector
-	/// </summary>
-	public float MaxLatitudeUp {
-		get {
-			return this._maxLatitudeUp;
-		}
-		set {
-			_maxLatitudeUp = value;
-		}
-	}
-	#endregion
+    [SerializeField]
+    float _radius = 0.4f;       // Jitter length 
 
-	
-	protected override Vector3 CalculateForce ()
-	{
-		float speed = Vehicle.MaxSpeed;
+    [SerializeField]
+    float _angleMax = 5f;       // Maximum jitter angle change per tick in degrees.
 
-		// random walk WanderSide and WanderUp between -1 and +1
-		_wanderSide = OpenSteerUtility.scalarRandomWalk (_wanderSide, speed, -_maxLatitudeSide, _maxLatitudeSide);
-		_wanderUp   = OpenSteerUtility.scalarRandomWalk (_wanderUp,   speed, -_maxLatitudeUp,   _maxLatitudeUp);
-		
-		// return a pure lateral steering vector: (+/-Side) + (+/-Up)
-		Vector3	 result = (transform.right * _wanderSide) + (transform.up * _wanderUp);
-		return result;
-	}
-	
+    [SerializeField]
+    float _distance = 0.6f;     // Offset distance of jitter
+
+    #region Public properties
+
+    /// <summary>
+    /// Length of the jitter force.
+    /// </summary>
+    public float Radius
+    {
+        get
+        {
+            return _radius;
+        }
+        set
+        {
+            _radius = value;
+        }
+    }
+
+    /// <summary>
+    /// Maximum angle to adjust the jitter by in degrees.
+    /// </summary>
+    public float AngleMax
+    {
+        get
+        {
+            return _angleMax;
+        }
+        set
+        {
+            _angleMax = value;
+        }
+    }
+
+    /// <summary>
+    /// Offset of the jitter force along the vehicle's forward velocity.
+    /// </summary>
+    public float Distance
+    {
+        get
+        {
+            return _distance;
+        }
+        set
+        {
+            _distance = value;
+        }
+    }
+
+    #endregion
+
+    protected override Vector3 CalculateForce()
+    {
+        // Increment the angle
+        _angle += _angleMax * Random.Range(-1f, 1f);
+
+        // Place the base force out in front of the agent
+        var baseForce = Vehicle.Speed == 0
+            ? Vehicle.transform.forward.normalized  // Default to forward vector
+            : Vehicle.NormalizedVelocity;           // Velocity
+        
+        // Rotate the jitter angle about the up vector.
+        _jitter = Quaternion.AngleAxis(_angle, Vector3.up) * baseForce * _radius;
+
+        return baseForce * _distance + _jitter;
+    }
+
+#if ANNOTATE_WANDER
+
+    void OnDrawGizmos()
+    {
+        if (Vehicle != null)
+        {
+            var baseForce = Vehicle.Position + (Vehicle.NormalizedVelocity * _distance);
+
+            Debug.DrawLine(Vehicle.Position, baseForce, Color.black);
+            Debug.DrawLine(baseForce, baseForce + _jitter, Color.red);
+        }
+    }
+
+#endif
 }
-
