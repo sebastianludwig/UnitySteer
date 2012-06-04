4701f2150647646f03cb9db8835ca5208f6e97a8
Sebastian Ludwig <sebastian@lurado.de>
2012-06-05 00:14:12 +0200

    merged e3a0881ebc10f34c37789485abeae7a1913a056f from https://github.com/leniency/UnitySteer/
    "Changed up Tether. Rather than do a hard reflect, it allows a cushion zone between an inner and outer radius. Plus, it now actually makes use of the steering weight. It applies a percentage of the steering weight to the vehicle that scales as it approaches the outer radius (past there it continues to scale above 100%). Allows for softer course adjustments. The hard reflect could be added back in at the outer radius easily enough and still leave the softer inner redirection. Seems to work nicest when the tether weight is small."

--- Behaviors/SteerForTether.cs
+++ Behaviors/SteerForTether.cs
@@ -1,3 +1,4 @@
+#define ANNOTATE_TETHER
 using UnityEngine;
 using UnitySteer.Helpers;
 
@@ -7,47 +8,105 @@ using UnitySteer.Helpers;
 [AddComponentMenu("UnitySteer/Steer/... for Tether")]
 public class SteerForTether : Steering
 {
-	#region Private properties
-	[SerializeField]
-	float _maximumDistance = 30f;
-	[SerializeField]
-	Vector3 _tetherPosition;
-	#endregion
-	
-	
-	#region Public properties
-	public float MaximumDistance {
-		get {
-			return this._maximumDistance;
-		}
-		set {
-			_maximumDistance = Mathf.Clamp(value, 0, float.MaxValue);
-		}
-	}
+    #region Private properties
 
-	public Vector3 TetherPosition {
-		get {
-			return this._tetherPosition;
-		}
-		set {
-			_tetherPosition = value;
-		}
-	}
-	#endregion
-	
+    [SerializeField]
+    float _outerRadius = 30f;
 
-	
-	protected override Vector3 CalculateForce ()
-	{
-		Vector3 steering = Vector3.zero;
-		
-		var difference = TetherPosition - Vehicle.Position;
-		var distance = difference.magnitude;
-		if (distance > _maximumDistance)
-		{
-			steering = difference - Vehicle.Velocity;
-		}
-		return steering;
-	}
+    [SerializeField]
+    float _innerRadius = 20f;
+
+    [SerializeField]
+    Vector3 _position;
+
+    #endregion
+
+    #region Public properties
+
+    /// <summary>
+    /// The maximum radius of the tether.  The pushback force is maximal here.
+    /// </summary>
+    public float OuterRadius
+    {
+        get
+        {
+            return this._outerRadius;
+        }
+        set
+        {
+            _outerRadius = Mathf.Clamp(value, 0, float.MaxValue);
+        }
+    }
+
+    /// <summary>
+    /// The inner radius of the tether where the pushback force starts.
+    /// The pushback increases as the vehicle approaches the outer radius.
+    /// </summary>
+    public float InnerRadius
+    {
+        get
+        {
+            return this._innerRadius;
+        }
+        set
+        {
+            _innerRadius = Mathf.Clamp(value, 0, float.MaxValue);
+        }
+    }
+
+    /// <summary>
+    /// The center position of the tether.
+    /// </summary>
+    public Vector3 Position
+    {
+        get
+        {
+            return this._position;
+        }
+        set
+        {
+            _position = value;
+        }
+    }
+
+    #endregion
+
+    protected override Vector3 CalculateForce()
+    {
+        Vector3 steering = Vector3.zero;
+
+        var difference = Position - Vehicle.Position;   // vector difference between vehicle and tether.
+        var distance = difference.magnitude;
+
+        // Check if the vehicle is outside the inner radius.  If so,
+        // start applying pushback force.
+        if (distance > _innerRadius)
+        {
+            float percent = 0;
+
+            if (_outerRadius == _innerRadius)
+            {
+                percent = 1;
+            }
+            else
+            {
+                // Percent = 
+                percent = (distance - _innerRadius) / (_outerRadius - _innerRadius);
+            }
+
+            steering = difference * percent;
+        }
+
+        return steering;
+    }
+
+#if ANNOTATE_TETHER
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0f, 0f, 1f, 0.9f);
+        Gizmos.DrawWireSphere(_position, _outerRadius);
+        Gizmos.color = new Color(0f, 0f, 0.8f, 0.9f);
+        Gizmos.DrawWireSphere(_position, _innerRadius);
+    }
+#endif
 }
-
