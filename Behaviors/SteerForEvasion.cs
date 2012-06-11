0451657f44f9d3e058d93ee76acf57a686443e5f
Sebastian Ludwig <sebastian@lurado.de>
2012-06-11 18:10:51 +0200

    - CHANGED SteerForEvasion uses new MovementTracker to be able to escape not only vehicles, but any GameObject

--- Behaviors/SteerForEvasion.cs
+++ Behaviors/SteerForEvasion.cs
@@ -3,17 +3,23 @@ using UnitySteer;
 using UnitySteer.Helpers;
 
 /// <summary>
-/// Steers a vehicle to avoid another one
+/// Steers a vehicle to avoid another object. The objects velocity is tracked by
+/// a MovementTracker.
 /// </summary>
 [AddComponentMenu("UnitySteer/Steer/... for Evasion")]
 public class SteerForEvasion : Steering
 {
 	#region Private fields
+	MovementTracker _movementTracker;
+	
 	[SerializeField]
-	Vehicle _menace;
+	Transform _menace;
 
 	[SerializeField]
 	float _predictionTime;
+	
+	[SerializeField]
+	float _trackingInterval = 0.25f;
 	#endregion
 	
 	#region Public properties
@@ -32,28 +38,43 @@ public class SteerForEvasion : Steering
 	/// <summary>
 	/// Vehicle menace
 	/// </summary>
-	public Vehicle Menace {
+	public Transform Menace {
 		get {
 			return this._menace;
 		}
 		set {
 			_menace = value;
+			if (_movementTracker != null)
+			{
+				_movementTracker.target = _menace;
+				_movementTracker.enabled = _menace != null;
+			}
 		}
 	}
 	#endregion
 	
+	protected override void Awake()
+	{
+		base.Awake();
+		_movementTracker = new MovementTracker(Menace, _trackingInterval);
+		_movementTracker.enabled = Menace != null;
+	}
+	
 	protected override Vector3 CalculateForce()
 	{
+		if (_menace == null)
+		{
+			return Vector3.zero;
+		}
+		
 		// offset from this to menace, that distance, unit vector toward menace
-		Vector3 offset = _menace.Position - Vehicle.Position;
+		Vector3 offset = _menace.position - Vehicle.Position;
 		float distance = offset.magnitude;
 
-		float roughTime = distance / _menace.Speed;
-		float predictionTime = ((roughTime > _predictionTime) ?
-									  _predictionTime :
-									  roughTime);
-
-		Vector3 target = _menace.PredictFuturePosition(predictionTime);
+		float roughTime = _movementTracker.isMoving ? distance / _movementTracker.velocity.magnitude : 0;
+		float predictionTime = Mathf.Min(roughTime, _predictionTime);
+		
+		Vector3 target = Menace.position + _movementTracker.velocity * predictionTime;
 
 		// This was the totality of SteerToFlee
 		Vector3 desiredVelocity = Vehicle.Position - target;
